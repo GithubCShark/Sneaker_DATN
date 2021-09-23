5f3893cdb0dc70df6970536c5455686e4ae00359
NgDam <NgDam@ALIENLYS>
2021-09-23 20:57:41 +0700

    Add project files.

---
+++ Sneaker_DATN/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sneaker_DATN.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
