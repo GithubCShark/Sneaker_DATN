b380a11f9e521f3a6a22c1be804e3be27425f20c
Mtab <anh.mt1602@gmail.com>
2021-11-02 20:05:14 +0700

    Edit Chart (error)

--- Sneaker_DATN/Controllers/ChartController.cs
+++ Sneaker_DATN/Controllers/ChartController.cs
@@ -1,18 +1,40 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Sneaker_DATN.Models;
+using Sneaker_DATN.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Web.Mvc;
+using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
 
 namespace Sneaker_DATN.Controllers
 {
     public class ChartController : BaseController
     {
-        // GET: ChartController
+        private DataContext _context;
+        public ChartController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetAllDateUserMem(int year)
+        {
+            List<Users> list = new List<Users>();
+            list = _context.Users.Where(p => p.RoleID.Equals(3)).ToList();
+            var listcount = from p in list
+                            where p.DateCreated.Value.Year == year
+                            group p by p.DateCreated.Value.Month into List
+                            select new
+                            {
+                                Thang = List.Key,
+                                SoLuong = List.Count()
+                            };
+            return Json(listcount, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Sneaker_DATN/Services/ChartSvc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Sneaker_DATN.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sneaker_DATN.Services
+{
+    //public interface IChartSvc
+    //{
+    //    int GetDateAllUserMem();
+    //    //List<Orders> GetDateAllOrder(DateTime year);
+    //}
+    //public class ChartSvc : IChartSvc
+    //{
+    //    protected DataContext _context;
+    //    public ChartSvc(DataContext dataContext)
+    //    {
+    //        _context = dataContext;
+    //    }
+
+    //    public List<> GetDateAllUserMem()
+    //    {
+    //        List<Users> list = new List<Users>();
+    //        list = _context.Users.ToList();
+    //        var listcount = from p in list
+    //                        where p.DateCreated.Value.Year == 2021
+    //                        group p by p.DateCreated.Value.Month into List
+    //                        select new
+    //                        {
+    //                            Thang = List.Key,
+    //                            SoLuong = List.Count()
+    //                        };
+    //        return listcount;
+    //    }
+    //}
+}
