3c6f5eeea89e1a9490dab8698d6fa04bb8abe19b
Mtab <anh.mt1602@gmail.com>
2021-11-05 04:06:29 +0700

    Edit Chart (rollback)

--- Sneaker_DATN/Controllers/ChartController.cs
+++ Sneaker_DATN/Controllers/ChartController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Sneaker_DATN.Models;
 using Sneaker_DATN.Services;
@@ -6,7 +7,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
 
 namespace Sneaker_DATN.Controllers
@@ -22,12 +22,13 @@ namespace Sneaker_DATN.Controllers
         {
             return View();
         }
-        public ActionResult GetAllDateUserMem(int year)
+        [System.Web.Mvc.HttpPost]
+        public Microsoft.AspNetCore.Mvc.JsonResult GetAllDateUserMem()
         {
             List<Users> list = new List<Users>();
             list = _context.Users.Where(p => p.RoleID.Equals(3)).ToList();
             var listcount = from p in list
-                            where p.DateCreated.Value.Year == year
+                            where p.DateCreated.Value.Year == 2021
                             group p by p.DateCreated.Value.Month into List
                             select new
                             {
