c4f180ee17b0a22e996e3a168a98adf931a5fea4
Mtab <anh.mt1602@gmail.com>
2021-11-06 15:01:14 +0700

    Edit Chart (test)

--- Sneaker_DATN/Controllers/ChartController.cs
+++ Sneaker_DATN/Controllers/ChartController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System.Web.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Sneaker_DATN.Models;
 using Sneaker_DATN.Services;
@@ -7,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ActionResult = Microsoft.AspNetCore.Mvc.ActionResult;
+using Newtonsoft.Json;
 
 namespace Sneaker_DATN.Controllers
 {
@@ -18,24 +17,35 @@ namespace Sneaker_DATN.Controllers
         {
             _context = dataContext;
         }
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
-        [System.Web.Mvc.HttpPost]
-        public Microsoft.AspNetCore.Mvc.JsonResult GetAllDateUserMem()
+        [HttpPost]
+        public IActionResult GetAllDateUserMem()
         {
             List<Users> list = new List<Users>();
             list = _context.Users.Where(p => p.RoleID.Equals(3)).ToList();
             var listcount = from p in list
                             where p.DateCreated.Value.Year == 2021
                             group p by p.DateCreated.Value.Month into List
-                            select new
+                            select  new
                             {
                                 Thang = List.Key,
                                 SoLuong = List.Count()
                             };
-            return Json(listcount, JsonRequestBehavior.AllowGet);
+            List<ViewChar> dataChart = new List<ViewChar>();
+            foreach (var item in listcount)
+            {
+                dataChart.Add(new ViewChar
+                {
+                    Thang = item.Thang,
+                    SoLuong = item.SoLuong
+                });
+            }
+            //return View(dataChart);
+            return View(JsonConvert.SerializeObject(dataChart));
+            //return Json(listcount, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Sneaker_DATN/Models/ViewModels/ViewChart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sneaker_DATN.Models
+{
+    public class ViewChar
+    {
+        public int Thang { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
