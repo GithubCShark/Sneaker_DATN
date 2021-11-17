1fc5af5c54716ba994fea68f794e2927d0970ffd
Mtab <anh.mt1602@gmail.com>
2021-11-18 02:37:20 +0700

    Add Report (error), Edit Chart

---
+++ Sneaker_DATN/Controllers/ReportController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sneaker_DATN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sneaker_DATN.Controllers
+{
+    public class ReportController : Controller
+    {
+        private DataContext _context;
+        public ReportController(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        // GET: ReportController
+        public IActionResult Index()
+        {
+            var lsProduct = from x in _context.Products
+                         select x;
+            var lsOrderDetails = from y in _context.OrderDetails
+                                 select y;
+
+            var lsorderd = from y in _context.OrderDetails
+                           select y.ProductID;
+            var lsprod = lsProduct.Where(x => lsorderd.Contains(x.ProductID));
+
+            List<ViewReportProducts> lsReport = new List<ViewReportProducts>();
+            foreach (var item in lsprod)
+            {
+                var lsquantity = from x in lsOrderDetails
+                                 where x.ProductID == item.ProductID
+                                 select x.Quantity;
+                var lstotal = from x in lsOrderDetails
+                              where x.ProductID == item.ProductID
+                              select x.Price;
+
+                lsReport.Add(new ViewReportProducts
+                {
+                    Products = item,
+                    Quantity = lsquantity.Sum(),
+                    Total = lstotal.Sum()
+                });
+            }
+            var lsrp = lsReport.OrderBy(x => x.Products.ProductID).ToList();
+
+            return View(lsrp);
+        }
+    }
+}
---
+++ Sneaker_DATN/Models/ViewModels/ViewReportProducts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sneaker_DATN.Models
+{
+    public class ViewReportProducts
+    {
+        public Products Products { get; set; }
+
+        [Display(Name = "Số lượng")]
+        public int Quantity { get; set; }
+
+        [Display(Name = "Tổng tiền")]
+        public double Total { get; set; }
+    }
+}
