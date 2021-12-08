dfb810db40de35d536f3100182a628d5246f52eb
HuyL4m <huy.lamgia1601@gmail.com>
2021-12-08 10:48:35 +0700

    update css custom product

--- Sneaker_DATN/wwwroot/js/main.js
+++ Sneaker_DATN/wwwroot/js/main.js
@@ -129,3 +129,16 @@ window.addEventListener("load", function () {
         line.style.top = 0;
     })
 })
+
+
+/*sale off index*/
+setInterval(() => {
+    let date = new Date(),
+        hour = date.getHours(),
+        min = date.getMinutes(),
+        sec = date.getSeconds();
+
+    document.querySelector(".hour_num").innerText = hour;
+    document.querySelector(".min_num").innerText = min;
+    document.querySelector(".sec_num").innerText = sec;
+}, 1000);
